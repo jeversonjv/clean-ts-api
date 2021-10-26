1bd192e11f59feaa8e71425fdd60dba6b9bb03f9
Jeverson <jeversontp@gmail.com>
2021-10-25 21:46:21 -0300

    test: ensure AccountMongoRepository returns and account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,27 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_mail@mail.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    console.log('🚀 ~ file: account.spec.ts ~ line 21 ~ test ~ account', account)
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_mail@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
