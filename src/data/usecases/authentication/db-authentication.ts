749175947a06d92ad5ae8db96c90c0069962a795
Jeverson <jeverson.goncalves@usemobile.xyz>
2022-03-16 10:52:08 -0300

    feat: ensure DbAuthentication returns a token on success

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -18,11 +18,15 @@ export class DbAuthentication implements Authentication {
       authentication.email
     )
 
-    if (account) {
-      await this.hashComparer.compare(authentication.password, account.password)
-      await this.tokenGenerator.generate(account.id)
-    }
+    if (!account) return null
 
-    return null
+    const isValid = await this.hashComparer.compare(
+      authentication.password,
+      account.password
+    )
+
+    if (!isValid) return null
+
+    return this.tokenGenerator.generate(account.id)
   }
 }
