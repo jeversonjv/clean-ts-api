9606b804948cbff61b2fc5f10560843f76164a66
Jeverson <jeverson.goncalves@usemobile.xyz>
2022-03-19 16:37:46 -0300

    feat: add Login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-ts-api',
-  port: process.env.PORT ?? 5050
+  port: process.env.PORT ?? 5050,
+  jwtSecret: process.env.JWT_SECRET ?? '9h2#%3Z+hC(W[ML'
 }
