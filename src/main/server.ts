2dcec59711e315ceaa3a4d59a36b9da166434beb
Jeverson <jeverson.goncalves@usemobile.xyz>
2022-03-19 17:33:44 -0300

    chore: add dotenv

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,10 @@
+import 'dotenv/config'
 import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 import env from './config/env'
 
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
-    const app = (await import ('./config/app')).default
+    const app = (await import('./config/app')).default
     app.listen(env.port, () => {
       console.log(`Server is running on port ${env.port}`)
     })
