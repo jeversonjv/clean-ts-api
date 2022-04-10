47e72a93edcdc8061419704e032b41e78136036c
jeversonjv <jeversontp@gmail.com>
2022-04-10 20:54:08 -0300

    refactor: allows api to run locally or with docerk

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://mongo:27017/clean-ts-api',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-ts-api',
   port: process.env.PORT ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? '9h2#%3Z+hC(W[ML'
 }
