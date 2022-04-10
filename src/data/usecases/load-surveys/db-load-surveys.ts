b32ea4d6791de41820a763bb1b30261b75a8c0b2
jeversonjv <jeversontp@gmail.com>
2022-04-10 12:54:26 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,13 @@
+import {
+  LoadSurveys,
+  LoadSurveysRepository,
+  SurveyModel
+} from './db-load-surveys-protocols'
+
+export class DbLoadSurveys implements LoadSurveys {
+  constructor(private readonly loadSurveysRepository: LoadSurveysRepository) {}
+
+  async load(): Promise<SurveyModel[]> {
+    return this.loadSurveysRepository.loadAll()
+  }
+}
