5319fb5f34227abb0dd7bd063c8b79877f8cea5d
Jeverson <jeversontp@gmail.com>
2022-04-16 21:06:30 -0300

    test: ensure save survey result route returns 403 if no token is provided

---
+++ src/main/routes/survey-result-routes.test.ts
@@ -0,0 +1,76 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+// import { sign } from 'jsonwebtoken'
+// import env from '@/main/config/env'
+
+let surveyCollection: Collection
+let accountCollection: Collection
+
+/*
+const makeFakeBodyRequest = (): any => ({
+  question: 'any_question',
+  answers: [
+    {
+      image: 'http://any_image.com',
+      answer: 'any_answer'
+    },
+    {
+      answer: 'other_answer'
+    }
+  ]
+})
+
+const makeAccessToken = async (role?: string): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'Jeverson',
+    email: 'jeversontp@gmail.com',
+    password: 'any_password',
+    role
+  })
+
+  const id = res.insertedId
+  const accessToken = sign({ id }, env.jwtSecret)
+
+  await accountCollection.updateOne(
+    { _id: id },
+    {
+      $set: {
+        accessToken
+      }
+    }
+  )
+
+  return accessToken
+} */
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = MongoHelper.getCollection('surveys')
+    accountCollection = MongoHelper.getCollection('accounts')
+    await Promise.all([
+      surveyCollection.deleteMany({}),
+      accountCollection.deleteMany({})
+    ])
+  })
+
+  describe('PUT /surveys/:surveyId/results', () => {
+    test('Should return 403 on save survey result without access token', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(403)
+    })
+  })
+})
